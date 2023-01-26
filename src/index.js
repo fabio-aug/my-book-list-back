496778de01cc7bf2b0673c256f012b9e0dbf271d
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-01-26 17:13:41 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 
+
 // Local
 const { database } = require('./database/Database');
 const userController = require('./controllers/User.controller');
@@ -19,12 +20,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(userController);
 app.use(bookController);
 
+
 // Conectar com banco de dados
 database.authenticate().then(() => {
     console.log('Conectado ao banco com sucesso!');
 }).catch(() => {
     console.log('Erro ao conectar ao banco.');
 });
+
 
 // Inicialização do servidor
 const port = 44390;
