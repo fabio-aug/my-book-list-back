fe450799630f181a07cda0d46e09055903ac54a3
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-02-20 09:01:25 +0000

    Request de dashboard

--- src/controllers/Review.controller.js
+++ src/controllers/Review.controller.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const fr = require('./../utils/FormatResponse');
 const reviewService = require('./../services/Review.service');
 
 const reviewController = express.Router();
-
 
 reviewController.get('/review/getMostReviewed', (req, res, next) => {
     // #swagger.tags = ['review']
@@ -24,6 +24,48 @@ reviewController.get('/review/getBestReviewed', (req, res, next) => {
         console.error("Erro ao buscar livros.");
         next(error);
     });
+});
+
+reviewController.get('/review/dashboardByIdUser', (req, res, next) => {
+    // #swagger.tags = ['review']
+    // #swagger.description = 'Buscar informaçãoes da lista de reviews do usuário pelo ID.'
+
+    /* #swagger.parameters['idUser'] = {
+        required: true,
+        type: 'integer',
+        description: 'Id do usuário a buscar informações.'
+    } */
+
+    reviewService.dashboardByIdUser(req.query.idUser).then((response) => {
+        const data = fr.responseSucces(response);
+        res.send(data);
+    }).catch((error) => {
+        const data = fr.responseError(error.message);
+        res.status(500).send(data);
+    });
+
+    
+
+    /* #swagger.responses[200] = { 
+        schema: {
+            data: {
+                reading: 0,
+                completed: 0,
+                stopped: 0,
+                toRead: 0
+            },
+            status: true
+        },
+        description: 'Sucesso.' 
+    } */
+
+    /* #swagger.responses[500] = { 
+        schema: {
+            msg: 'Mensagem de erro.',
+            status: false
+        },
+        description: 'Erro.' 
+    } */
 });
 
 module.exports = reviewController;
