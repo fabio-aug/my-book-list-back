0899d512a1ccfe5e2dd52fd4ff28e200f3f2cf10
DESKTOP-QOO7NF7\luana <luanaassis0908@gmail.com>
2023-02-20 09:50:01 -0300

    hf

--- src/database/Database.js
+++ src/database/Database.js
@@ -1,11 +1,7 @@
 const sequelize = require('sequelize');
 /* https://sequelize.org/docs/v6/ */
 
-<<<<<<< HEAD
 const database = new sequelize('MyBookList', '<user_name>', '<user_password>', {
-=======
-const database = new sequelize('MyBookList', '<name>', '<password>', {
->>>>>>> f763be9e6fdd7d4b858e89495f249367e043a076
     dialect: 'mysql',
     host: 'localhost'
 });
